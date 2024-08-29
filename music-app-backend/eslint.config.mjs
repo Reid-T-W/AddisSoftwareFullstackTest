8477dcfa56bcd37342314a3aa5e51265f745d0fd
Reid-T-W <rediettadesse100@gmail.com>
2024-08-29 17:58:28 +0300

    config: lintstage and eslint security plugin added

--- music-app-backend/eslint.config.mjs
+++ music-app-backend/eslint.config.mjs
@@ -2,17 +2,22 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginPrettier from "eslint-plugin-prettier";
 import prettierRecommended from "eslint-config-prettier";
+import pluginSecurity from "eslint-plugin-security";
 
 
 export default [
   prettierRecommended,
+  pluginSecurity.configs.recommended,
   {
     files: ["**/*.js"],
     plugins: {
       prettier: pluginPrettier,
+    security: pluginSecurity,
     },
     rules: {
+      "no-console": "error",
       "consistent-return": "off",
+      "security/detect-object-injection": "off",
     },
     languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
