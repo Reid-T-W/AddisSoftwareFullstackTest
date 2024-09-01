2d7797b1fed035734bfbb6c56e86bd98a959ce7e
Reid-T-W <rediettadesse100@gmail.com>
2024-09-01 23:12:56 +0300

    docs: added swagger documentation for stats route

---
+++ music-app-backend/src/utils/swagger/docs/stats.js
@@ -0,0 +1,14 @@
+// GET ALL STATS
+
+/**
+ * @openapi
+ * /stats:
+ *  get:
+ *   tags:
+ *     - Stats
+ *   summary: Get stats
+ *   description: Get all stats
+ *   responses:
+ *    200:
+ *      description: Stats fetched successfully
+ */
