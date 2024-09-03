b51184bd31614bc2f9dc97dfb67aa5c3fae8bf3d
Reid-T-W <rediettadesse100@gmail.com>
2024-09-03 17:19:25 +0300

    feature: album endpoint supports search by album or artist name

--- music-app-backend/src/controllers/album.controller.js
+++ music-app-backend/src/controllers/album.controller.js
@@ -5,14 +5,31 @@ const catchAsync = require("../utils/catchAsync");
 const getAlbums = catchAsync(async (req, res, next) => {
     const { search } = req.query;
     if (search) {
-      const query = {
-        $or: [
-        //   { album: new RegExp(search, 'i') },
-        //   { artist: new RegExp(search, 'i') },
-        ]
-      };
-      const songs = await Song.find(query).sort({ createdAt: -1 });
-      res.json(songs);
+        const albums = await Song.aggregate([
+            {      
+                $match: {
+                    $or: [
+                        { artist: new RegExp(search, 'i') },
+                        { album: new RegExp(search, 'i') },
+                    ]
+                },
+            },
+            {
+                $group: {
+                  _id: { artist: '$artist', album: '$album' },
+                  noOfSongs: { $sum: 1 },
+                },
+              },
+              {
+                $project: {
+                  artist: '$_id.artist',
+                  album: '$_id.album',
+                  noOfSongs: 1,
+                  _id: 0,
+                },
+              },
+          ]);
+      res.json(albums);
     } else {
         const albums = await Song.aggregate([
             {
