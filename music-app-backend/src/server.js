05e8bf37f68b3e3b9ad43722e63cd30148da6e52
Reid-T-W <rediettadesse100@gmail.com>
2024-09-03 20:19:49 +0300

    enhancement: updated cors rule

--- music-app-backend/src/server.js
+++ music-app-backend/src/server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:4173'],
 };
 app.use(cors(corsOptions));
 app.use(express.json());
